7fec01b968255e37e45e64aee38b8748337a0628
kalyan poshala <kalyanposhala@gmail.com.com>
2019-10-07 10:55:49 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNonNumeric
+{
+    public class TextInput
+    {
+        public string str;
+        public string value;
+
+
+        public virtual void Add(char c)
+        {
+            string str = c.ToString();
+        }
+
+        public string GetValue()
+        {
+            //this.Add();
+            var returnstring1= value;
+            return returnstring1;
+        }
+    }
+
+    public class NumericInput : TextInput
+    {
+        public override void Add(char c)
+        {
+
+            var nonNumeric = Char.IsNumber(c);
+            if (nonNumeric)
+            {
+                 value = value +""+ c;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TextInput input = new NumericInput();
+            input.Add('1');
+            input.Add('a');
+            input.Add('0');
+            Console.WriteLine(input.GetValue());
+            Console.ReadLine();
+        }
+    }
+}
